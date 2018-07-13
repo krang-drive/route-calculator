777f195b3ef99d9f92f10afbcf2071a70ad93b28
Devon Ray <rayd3@montclair.edu>
2018-07-13 09:55:11 -0400

    big old tarball of, oh wait... commented out stuff that didnt work :(

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 var bodyParser = require('body-parser');
-var Client = require('node-rest-client').Client;
+var client = require('sync-rest-client');
  
-var client = new Client();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -29,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
  * }
  * */
 app.post('/api', function(req, res) {
-	var facilityId = req.body.facility.facilityId;
+	/* var facilityId = req.body.facility.facilityId;
 	var facilityLocation = req.body.facility.facilityLocation;
 	var packages = req.body.packages;
 	
@@ -45,7 +43,9 @@ app.post('/api', function(req, res) {
 			packagesChunk.push(packages[i]);
 		}
 		else {
-			routes.push(calculateRoute(packagesChunk, facilityLocation, facilityId));
+			console.log("before");
+			routes.push(calculateRoute(packagesChunk, facilityLocation, facilityId)[0]);
+			console.log("after");
 			chunkIndex = 0;
 			packagesChunk = [];
 		}
@@ -53,62 +53,70 @@ app.post('/api', function(req, res) {
 	
 	// Check for the last chunk < 8 packages
 	if (Array.isArray(packagesChunk) && packagesChunk.length) {
-		routes.push(calculateRoute(packagesChunk, facilityLocation, facilityId))
+		routes.push(calculateRoute(packagesChunk, facilityLocation, facilityId)[0])
 	}
 	var result = {
 		deliveryRoutes: routes,
 		facilityId: facilityId
 	};
-	console.log("Result: " + result)
-	res.send(result);
+	
+	console.log("Result: " + JSON.stringify(result)) */
+	
+	var routeResult = {
+		deliveryRoutes: [
+			{
+				driverId: "-1",
+				facilityId: 4,
+				routeId: Math.random()*1000,
+				googleMapsLink: "https://www.google.com/maps/dir/340+MacArthur+Blvd,+Mahwah,+NJ+07430/UPS+Innovation+Center,+Parsippany-Troy+Hills,+NJ/The+UPS+Store,+Rockaway,+NJ/123+South+Main+Street,+Lodi,+NJ/340+MacArthur+Boulevard,+Mahwah,+NJ/",
+				bounty: 42
+			},
+			{
+				driverId: "-1",
+				facilityId: 4,
+				routeId: Math.random()*1000,
+				googleMapsLink: "https://www.google.com/maps/dir/340+MacArthur+Blvd,+Mahwah,+NJ+07430/UPS+Innovation+Center,+Parsippany-Troy+Hills,+NJ/The+UPS+Store,+Rockaway,+NJ/123+South+Main+Street,+Lodi,+NJ/340+MacArthur+Boulevard,+Mahwah,+NJ/",
+				bounty: 42
+			}
+		]
+	};
+	
+	res.send(routeResult);
 });
 
-function calculateRoute(packagesChunk, facilityLocation, facilityId) {
+/* function calculateRoute(packagesChunk, facilityLocation, facilityId) {
+	console.log("during");
 	var apiKey = "AIzaSyBINdf7SLAlb6MwSMGEhYaRTgAcSet_Qno"
 	
 	var deliveryLocations = []
 	
+	console.log("Packages chunk: " + JSON.stringify(packagesChunk));
+	
 	for(var i = 0; i < packagesChunk; i++) {
-		var package = packagesChunk[i];
-		var deliveryLocation = package.deliveryLocation;
+		var pkg = packagesChunk[i];
+		var deliveryLocation = pkg.deliveryLocation;
 		deliveryLocations.push(deliveryLocation);
 	}
 	
+	console.log("Delivery locations: " + deliveryLocations.join());
+	
 	var googleDirectionsRequest = "https://maps.googleapis.com/maps/api/directions/json?origin="+facilityLocation+"&destination="+facilityLocation+"&optimizeWaypoints=waypoints"+deliveryLocations.join("|")+"&key="+apiKey;
 	
-	var directions = "";
+	var googleMapsUrl = "https://www.google.com/maps/dir/"+"340 Macarthur Boulevard, Mahwah, NJ/"+deliveryLocations.join("/")+"340 Macarthur Boulevard, Mahwah, NJ"+"/";
+	console.log("URL: " + googleMapsUrl);
+	var routeId = Math.random()*1000;
+	console.log("Route id is "+ routeId);
+	var routeResult = {
+		driverId: "-1",
+		facilityId: facilityId,
+		routeId: Math.random()*1000,
+		googleMapsUrl: googleMapsUrl,
+		bounty: 42
+	};
 	
-	console.log("Google directions request URL: " + googleDirectionsRequest);
-	
-	return client.get(googleDirectionsRequest, function(data, response) {
-		console.log("data= " + data);
-		directions = data;
-		var indexOrder = directions.routes.waypoint_order;
-		var orderedLocations = [];
-		orderedLocations.push(facilityLocation);
-		if(!(Arrays.isArray(indexOrder) && !indexOrder.length))
-			for(var o = 0; o < indexOrder; o++)
-				orderedLocations.push(deliveryLocations[o]);
-		else {
-			orderedLocations = deliveryLocations;
-		}
-		orderedLocations.push(facilityLocation);
-		
-		var googleMapsUrl = "https://www.google.com/maps/dir/"+orderedLocations.join("/")+"/";
-		var routeId = Math.random()*1000;
-		console.log("Route id is "+ routeId);
-		var routeResult = {
-			driverId: "-1",
-			facilityId: facilityId,
-			routeId: Math.random()*1000,
-			googleMapsUrl: googleMapsUrl,
-			bounty: 42
-		};
-		return routeResult;
-	});
-	
-	
-}
+	console.log("Route result:" + JSON.stringify(routeResult));
+	return routeResult;	
+} */
 
 app.listen(port);
 
